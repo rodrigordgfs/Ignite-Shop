3e7917163d6f3fe33bd1daa04c0dd32462089bb6
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2023-03-02 22:59:44 -0300

    Feature: Imagens no Next.js

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from "next/app";
 import { globalStyles } from "../styles/global";
 import Logo from '../assets/logo.svg'
+import Image from "next/image";
 import { Container, Header } from "../styles/pages/app";
 
 globalStyles();
@@ -9,7 +10,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <Container>
       <Header>
-        <img src={Logo.src} />
+        <Image src={Logo} alt="" />
       </Header>
 
       <Component {...pageProps} />
