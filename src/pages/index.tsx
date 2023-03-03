150a8fa4888c57af2bdff5db553862cddf90902a
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2023-03-03 00:01:36 -0300

    Feature: Criando carrosel

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,47 @@
 import Image from "next/image";
-import { styled } from "../styles";
+
 import { HomeContainer, Product } from "../styles/pages/home";
+
+import { useKeenSlider } from 'keen-slider/react'
 
 import Camiseta1 from '../assets/camisetas/1.png'
 import Camiseta2 from '../assets/camisetas/2.png'
 import Camiseta3 from '../assets/camisetas/3.png'
 
+import 'keen-slider/keen-slider.min.css'
+
 export default function Home() {
+  const [sliderRef] = useKeenSlider({
+    slides: {
+      perView: 3,
+      spacing: 48,
+    }
+  })
+
   return (
-    <HomeContainer>
-      <Product>
+    <HomeContainer ref={sliderRef} className="keen-slider">
+      <Product className="keen-slider__slide">
         <Image src={Camiseta1} width={520} height={480} alt="" />
         <footer>
           <strong>Camista X</strong>
           <span>R$ 79,90</span>
         </footer>
       </Product>
-      <Product>
+      <Product className="keen-slider__slide">
+        <Image src={Camiseta2} width={520} height={480} alt="" />
+        <footer>
+          <strong>Camista X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
+        <Image src={Camiseta3} width={520} height={480} alt="" />
+        <footer>
+          <strong>Camista X</strong>
+          <span>R$ 79,90</span>
+        </footer>
+      </Product>
+      <Product className="keen-slider__slide">
         <Image src={Camiseta2} width={520} height={480} alt="" />
         <footer>
           <strong>Camista X</strong>
