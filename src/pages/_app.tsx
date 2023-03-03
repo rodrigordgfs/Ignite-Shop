821ee78d8d4ab47937fdf3d225154f2c3e1e4fc3
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2023-03-02 22:48:04 -0300

    Feature: Cabeçalho da aplicação

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,18 @@
 import type { AppProps } from "next/app";
 import { globalStyles } from "../styles/global";
+import Logo from '../assets/logo.svg'
+import { Container, Header } from "../styles/pages/app";
 
 globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Container>
+      <Header>
+        <img src={Logo.src} />
+      </Header>
+
+      <Component {...pageProps} />
+    </Container>
+  );
 }
