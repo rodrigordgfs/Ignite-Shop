dbb5847e36c472652bd236fc5d8a03e0506dd44f
Rodrigo Viegas Rodrigues <rodrigoviegas@lojaspompeia.com.br>
2023-04-11 02:03:43 -0300

    Feature: Alterado id dos produtps na pagina de produtos

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -72,7 +72,7 @@ export default function Product({ product }: ProductProps) {
 
 export const getStaticPaths: GetStaticPaths = async () => {
   return {
-    paths: [{ params: { id: "prod_MLH5Wy0Y97hDAC" } }],
+    paths: [{ params: { id: "prod_NSJpwHeTz9Zi2J" } }],
     fallback: "blocking",
   };
 };
